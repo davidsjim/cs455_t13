698f0e47f5d03c5555a5c1fc9b7e51d668806d71
Joseph Gelfand <j.n.gelfand@gmail.com>
2019-04-29 14:57:32 -0600

    Worked on massaging data into a normalizable form

--- kmeansClusterAll.py
+++ kmeansClusterAll.py
@@ -1,25 +1,51 @@
 from pyspark import SparkContext
 import json
+import functions
 
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 
+def columnizeTalentTree(tree):
+    return [int(t['val'].split('/')[0]) for t in tree['list']]
+
+def archerToColumn(char):
+    column=[]
+    column.append(char['character']['died']['times'] if 'died' in char['character'] else 0)
+    column.append(int(char['resources']['life'].split('/')[1]))
+    column.append(char['character']['level'])
+    for tree in ['Technique / Archery training', "Technique / Archery prowess", "Technique / Combat techniques",
+                 "Technique / Combat veteran", "Technique / Marksmanship", "Technique / Reflexes",
+                 "Technique / Munitions", "Technique / Agility", "Technique / Sniper", "Cunning / Trapping",
+                 "Technique / Combat training", "Technique / Mobility", "Cunning / Survival", "Technique / Conditioning"]:
+        column+=columnizeTalentTree(char['talents'][tree]) if tree in char['talents'] else ([0,0,0,0] if tree != "Technique / Combat training" else [0,0,0,0,0,0])
+    return column
+
 if __name__ == '__main__':
     sc = SparkContext()
-    rawData = sc.textFile("/cs455/project/data/chars")
 
-    # Filter characters for only Maj Eyal Campaign
-    data = rawData.filter(lambda line: len(line) > 1).map(lambda line: json.loads(line)).filter(
-        lambda char: 'character' in char).filter(lambda char: char['character']['campaign'] == "Maj'Eyal")
+    data=functions.readData(sc, "/cs455/project/data/chars")
+    data=functions.filterByAddons(data)
+    data=functions.filterByClass(data).persist()
 
-    # Valid addons are addons where 100 or more characters use the addon
-    # Filter by only valid addon characters
-    addons = data.flatMap(lambda char: char['character']['addons']).map(lambda addon: (addon, 1)).reduceByKey(lambda x, y: x + y)
-    addonsFiltered = addons.filter(lambda addon: addon[1] > 100)
+    archers=data.filter(lambda char: char['character']['class'] == 'Archer').map(archerToColumn)
+    archerList=archers.collect()
 
-    allowedAddons = set(addonsFiltered.map(lambda t: t[0]).collect())
-    chars = data.filter(lambda char: set(char['character']['addons']).issubset(allowedAddons)).persist()
+    levelIndex=2
+    levels={row[levelIndex] for row in archerList}
+    levelSums={level: ([0]*len(archerList[0], 0)) for level in levels}
+    for sum in levelSums:
+        sum.append([0]*len(archerList[0]))
 
-    # Only get classes with more than 300 characters to remove custom classes
-    charsByClass = chars.map(lambda char: (char['character']['class'], char)).filter(lambda char: char[1] > 300 and char[0] != "Tutorial Adventurer").collect()
+    for row in archerList:
+        level = row[levelIndex]
+        for i in range(len(row)):
+            if i == levelIndex:
+                levelSums[level][i]+=1
+                continue
+            if level > 50:
+                continue
+            levelSums[level][0][i] += row[i]
+            levelSums[level][1]=levelSums[level][1]+1
+
+
 
