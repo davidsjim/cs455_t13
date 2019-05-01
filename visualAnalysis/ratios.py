7d1138f80876b8eb45836e8dc11d4354248816c5
smilewood <milewood@gmail.com>
2019-05-01 16:55:26 -0600

    thing

--- visualAnalysis/ratios.py
+++ visualAnalysis/ratios.py
@@ -6,8 +6,6 @@ import sys
 blacklist = {'no_more_rares', 'bonusat10', 'cap-boost', 'expcontroller', 'starting-prodigy', 'Generous Levels', 'MoreGenLev','adventbuff'}
 
 if __name__ == '__main__':
-    archatypes = sys.argv[1].split(',')
-
     sc = SparkContext()
     rawData = sc.textFile("/cs455/project/data/chars")
     data = rawData.filter(lambda line: len(line) > 1).map(lambda line: json.loads(line)).filter(
@@ -23,24 +21,33 @@ if __name__ == '__main__':
     classesFiltered = classes.filter(lambda c: c[1] > 300 and c[0] != 'Tutorial Adventurer')
     allowedClasses = set(classesFiltered.map(lambda t: t[0]).collect())
 
-    if len(archatypes) == 0:
-        archatypes = allowedClasses
+    if len(sys.argv) > 1:
+        archetypes = sys.argv[1].split(',')
+    else:
+        archetypes = allowedClasses
 
     chars = data.filter(lambda char: set(char['character']['addons']).issubset(allowedAddons) and char['character']['class'] in allowedClasses).cache()
     data.unpersist()
 
-    usedClasses = chars.map(lambda char: ((char['character']['class'],  char['character']['level']), 1)).reduceByKey(lambda x,y:x+y).cache()
+    usedClasses = chars.map(lambda char: ((char['character']['class'],  char['character']['level']), 1)).reduceByKey(lambda x, y: x+y).cache()
+    badPractice = chars.flatMap(lambda char: [(l + 1, 1) for l in range(char['character']['level'])]).reduceByKey(
+            lambda x, y: x + y).collect()
+    totalChars = chars.count()
 
-    for c in archatypes:
-        levels = usedClasses.filter(lambda char: char[0][0] == c).collect()
-        ys = [char[1] for char in levels]
-        xs = [char[0][1] for char in levels]
-        plt.plot(xs, ys, 'o', label=c + " - " + str(sum([char[1] for char in levels])))
+    for c in archetypes:
+        aChars = usedClasses.filter(lambda char: char[0][0] == c).cache()
+
+        levels = aChars.flatMap(lambda char: [((l + 1), 1) for l in range(char[0][1])]).reduceByKey(lambda x, y: x + y).collect()
+
+        count = sum([x[1] for x in aChars.collect()])
+        ys = [(float(l[1])/float(count)) /
+              (float(sum([x[1] for x in badPractice if x[0] == l[0]])) / float(totalChars)) for l in levels]
+        print(float(levels[0][1]))
+        xs = [char[0] for char in levels]
+        plt.plot(xs, ys, '--o', label=c)
 
     plt.xticks([x + 1 for x in range(50)])
-    plt.title("Class popularity by level")
-    plt.xlabel("Level")
-    plt.ylabel("Number of characters in the class")
+    plt.ylim(bottom=0)
     plt.legend()
     plt.show()
 
